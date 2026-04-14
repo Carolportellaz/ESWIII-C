88054005dfc9cfc8a4047fb400d4b1e1af211735
Carol <carolineportellazanelato@gmail.com>
2026-04-14 11:48:17 -0300

    test: teste unitario de carrinho

---
+++ apps/api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.spec.ts"],
+  clearMocks: true,
+  moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.spec.json",
+      },
+    ],
+  },
+};
